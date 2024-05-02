73fa63c401c3fe2cce22f27215218cd686738095
CB009100 <97610809+nimnagunasekera@users.noreply.github.com>
2024-05-03 03:56:32 +0530

    added user pages

--- src/views/admin/user/Edit.vue
+++ src/views/admin/user/Edit.vue
@@ -1,8 +1,328 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div>
-    <h1>Edit page</h1>
-  </div>
+  <TransitionRoot as="template" :show="open">
+    <Dialog as="div" class="relative z-10" @close="open = true">
+      <TransitionChild
+        as="template"
+        enter="ease-out duration-300"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="ease-in duration-200"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div
+          class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+        >
+          <TransitionChild
+            as="template"
+            enter="ease-out duration-300"
+            enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enter-to="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leave-from="opacity-100 translate-y-0 sm:scale-100"
+            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <DialogPanel
+              class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6"
+            >
+              <form v-on:submit.prevent="saveUser">
+                <div>
+                  <div class="mt-3 sm:mt-5">
+                    <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">
+                      {{ user.name }}
+                    </DialogTitle>
+                    <div class="mt-2">
+                      <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-6">
+                        <div class="sm:col-span-8">
+                          <label
+                            for="name"
+                            class="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Name
+                          </label>
+                          <div class="mt-2">
+                            <div
+                              class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+                            >
+                              <input
+                                type="text"
+                                name="name"
+                                id="name"
+                                class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                v-model="user.name"
+                              />
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="sm:col-span-8">
+                          <label
+                            for="avatar"
+                            class="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Avatar
+                          </label>
+                          <div class="mt-2">
+                            <div
+                              class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+                            >
+                              <input
+                                type="text"
+                                name="avatar"
+                                id="avatar"
+                                class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                v-model="user.avatar"
+                              />
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="sm:col-span-4">
+                          <label
+                            for="email"
+                            class="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Email
+                          </label>
+                          <div class="mt-2">
+                            <div
+                              class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+                            >
+                              <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                v-model="user.email"
+                              />
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="sm:col-span-4">
+                          <label
+                            for="password"
+                            class="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Password
+                          </label>
+                          <div class="mt-2">
+                            <div
+                              class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+                            >
+                              <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                v-model="user.password"
+                              />
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="sm:col-span-4">
+                          <label
+                            for="age"
+                            class="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Age
+                          </label>
+                          <div class="mt-2">
+                            <div
+                              class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+                            >
+                              <input
+                                type="number"
+                                name="age"
+                                id="age"
+                                class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                v-model="user.age"
+                              />
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="sm:col-span-4">
+                          <label
+                            for="country"
+                            class="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Country
+                          </label>
+                          <div class="mt-2">
+                            <div
+                              class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+                            >
+                              <input
+                                type="text"
+                                name="country"
+                                id="country"
+                                class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                v-model="user.country"
+                              />
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="sm:col-span-4">
+                          <label
+                            for="subscription"
+                            class="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Subscription
+                          </label>
+                          <div class="mt-2">
+                            <div
+                              class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+                            >
+                              <input
+                                type="text"
+                                name="subscription"
+                                id="subscription"
+                                class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                v-model="user.subscription"
+                              />
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="sm:col-span-4">
+                          <label
+                            for="phone"
+                            class="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Phone
+                          </label>
+                          <div class="mt-2">
+                            <div
+                              class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+                            >
+                              <input
+                                type="tel"
+                                name="phone"
+                                id="phone"
+                                class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                v-model="user.phone"
+                              />
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="sm:col-span-4">
+                          <label
+                            for="sort_order"
+                            class="block text-sm font-medium leading-6 text-gray-900">
+                            Sort Order
+                          </label>
+                          <div class="mt-2">
+                            <div
+                              class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                              <input
+                                type="number"
+                                name="sort_order"
+                                id="sort_order"
+                                class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                v-model="user.sort_order"/>
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="sm:col-span-4">
+                          <label
+                            for="status"
+                            class="block text-sm font-medium leading-6 text-gray-900">
+                            Status
+                          </label>
+                          <div class="mt-2">
+                            <div
+                              class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                              <input
+                                type="checkbox"
+                                name="status"
+                                id="status"
+                                class="block flex-1 border-0 bg-transparent py-2 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 my-3"
+                                v-model="user.status"/>
+                            </div>
+                          </div>
+                        </div>
+
+                      </div>
+
+                      <div class="text-sm text-gray-500">
+                        <ul>
+                          <li>name: {{ user.name }}</li>
+                          <li>avatar: {{ user.avatar }}</li>
+                          <li>email: {{ user.email }}</li>
+                          <li>password: {{ user.password }}</li>
+                          <li>age: {{ user.age }}</li>
+                          <li>country: {{ user.country }}</li>
+                          <li>subscription: {{ user.subscription }}</li>
+                          <li>phone: {{ user.phone }}</li>
+                          <li>sort_order: {{ user.sort_order }}</li>
+                          <li>status: {{ user.status }}</li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="mt-5 sm:mt-6 flex gap-8">
+                  <RouterLink
+                    type="button"
+                    class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                    to="/admin/user"
+                    ref="cancelButtonRef"
+                  >
+                    Back
+                  </RouterLink>
+                  <button
+                    type="submit"
+                    class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
+                  >
+                    Save
+                  </button>
+                </div>
+              </form>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
 </template>
 
-<style></style>
+<script setup>
+import { ref } from 'vue'
+import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
+import { useRouter } from 'vue-router'
+
+const open = ref(true)
+
+const router = useRouter()
+
+const user = ref({
+  id: 1,
+  name: 'John Doe',
+  avatar: 'https://randomuser.me/api/portraits',
+  email: 'johndoe@example.com',
+  password: 'password',
+  age: 25,
+  country: 'United States',
+  subscription: 'Premium',
+  phone: '123-456-7890',
+  sort_order: 0,
+  status: true
+  })
+
+const saveUser = () => {
+  console.log(user)
+
+  // route the user to the user list page
+  router.push('/admin/user')
+}
+</script>
--- src/views/admin/user/Index.vue
+++ src/views/admin/user/Index.vue
@@ -1,9 +1,150 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div>
-    <h1>Users List page</h1>
-    <router-view />
+  <div class="main">
+    <div>
+      <SideBar />
+    </div>
+    <div class="px-4 sm:px-6 lg:px-8">
+      <div class="sm:flex sm:items-center">
+        <div class="sm:flex-auto">
+          <h1 class="text-base font-semibold leading-6 text-gray-900">Users</h1>
+          <p class="mt-2 text-sm text-gray-700">A list of all the users in this system.</p>
+        </div>
+        <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+          <RouterLink
+            to="/admin/user/create"
+            class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Add User
+          </RouterLink>
+        </div>
+      </div>
+      <div class="mt-8 flow-root">
+        <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table class="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                  >
+                    ID
+                  </th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Name
+                  </th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Avatar
+                  </th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Email
+                  </th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Subscription
+                  </th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Status
+                  </th>
+                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                    <span class="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="divide-y divide-gray-200">
+                <tr v-for="user in users" :key="user.id">
+                  <td
+                    class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0"
+                  >
+                    {{ user.id }}
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    {{ user.name }}
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    <img
+                      :src="user.avatar"
+                      alt="User avatar"
+                      class="h-8 w-8 rounded-full bg-gray-50"
+                    />
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    {{ user.email }}
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    {{ user.subscription }}
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    <span v-if="user.status">Active</span>
+                    <span v-else>Inactive</span>
+                  </td>
+                  <td
+                    class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0"
+                  >
+                    <RouterLink
+                      :to="`/admin/user/${user.id}`"
+                      class="text-indigo-600 hover:text-indigo-900"
+                    >
+                      View
+                      <span class="sr-only">, {{ user.name }}</span>
+                    </RouterLink>
+                    |
+                    <RouterLink
+                      :to="`/admin/user/${user.id}/edit`"
+                      class="text-indigo-600 hover:text-indigo-900"
+                    >
+                      Edit
+                      <span class="sr-only">, {{ user.name }}</span>
+                    </RouterLink>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+  <router-view />
 </template>
 
-<style></style>
+<script setup>
+import SideBar from '@/components/SideBar.vue';
+import { RouterLink } from 'vue-router';
+
+const users = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    subscription: 'Premium',
+    avatar: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    status: true
+  },
+  {
+    id: 2,
+    name: 'James Smith',
+    email: 'james@example.com',
+    subscription: 'Free',
+    avatar: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    status: false
+  },
+  {
+    id: 3,
+    name: 'Jerry West',
+    email: 'jerry@example.com',
+    subscription: 'Premium',
+    avatar: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    status: true
+  }
+]
+</script>
+<style>
+.main {
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  background: var(--color-background);
+  color: var(--color-text);
+  min-height: 100vh;
+}
+</style>
